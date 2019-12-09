ba900de67010f660441ab53196391d4a31ddfd8b
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2019-12-09 09:57:25 -0600

    Create SampleExtensions.js

---
+++ JavascriptExtensions/SampleExtensions.js
@@ -0,0 +1,7 @@
+function wGetDriverAhead(pos){
+	return  $prop('PersistantTrackerPlugin.DriverAhead_0'+pos+'_Name')
+} 
+
+function wGetDriverBehind(pos){
+	return  $prop('PersistantTrackerPlugin.DriverBehind_0'+pos+'_Name')
+} 
